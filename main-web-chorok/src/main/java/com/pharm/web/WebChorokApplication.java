6ea939498e137c52396d4d6163971fc84e0bbb2c
ahae75 <fun.ahae@gmail.com>
2021-02-07 23:20:14 +0900

    초기배포

---
+++ main-web-chorok/src/main/java/com/pharm/web/WebChorokApplication.java
@@ -0,0 +1,13 @@
+package com.pharm.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChorokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChorokApplication.class, args);
+	}
+
+}
---
+++ main-web-chorok/src/test/java/com/pharm/webchorok/WebChorokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pharm.webchorok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChorokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
